481509d3cd7e279675cfaf24dfecff7e59f472d4
Danissimos <skakovdh@mail.ru>
2019-12-18 18:05:00 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function f1()
+{
+    var element = document.getElementById('img');
+    element.style.backgroundImage = "url(fon1.jpg)";
+}
+function f2()
+{
+    var element = document.getElementById('img');
+    element.style.backgroundImage = "url(fon2.jpg)";
+}
+function f3()
+{
+    var element = document.getElementById('img');
+    element.style.backgroundImage = "url(fon3.jpg)";
+}
+function f4()
+{
+    var element = document.getElementById('img');
+    element.style.backgroundImage = "url(fon4.jpg)";
+}
+function f5()
+{
+    var element = document.getElementById('img');
+    element.style.backgroundImage = "url(fon5.jpg)";
+}
